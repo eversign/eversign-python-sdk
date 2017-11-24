beb539bad88e66aec977a24af16de82842784b4c
Clemens Ehrenreich <clemens@creativebrains.net>
2017-11-24 07:24:53 +0100

    WIP: oauth

---
+++ examples/oauth.py
@@ -0,0 +1,17 @@
+import sys
+import config
+from pprint import pprint
+sys.path.append("..")
+import eversign
+
+
+client = eversign.Client()
+
+options = {
+	'client_id': config.oauth_client_id,
+	'state': 'example'
+}
+
+url = client.generate_oauth_authorization_url(options)
+
+print('Please authorize: ' + url)
---
+++ examples/oauth_token.py
@@ -0,0 +1,18 @@
+import sys
+import config
+from pprint import pprint
+sys.path.append("..")
+import eversign
+
+
+client = eversign.Client()
+
+oauth_request = {
+	'client_id': config.oauth_client_id,
+	'client_secret': config.oauth_client_secret,
+	'code': config.code,
+	'state': config.state
+}
+
+token = client.request_oauth_token(oauth_request)
+client.set_oauth_token(token)
