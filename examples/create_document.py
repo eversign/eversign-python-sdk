3c0002ba2b98e6e9769063050b2204d1b4c398d2
Clemens Ehrenreich <ehrenreich@prescreen.io>
2018-09-27 15:45:31 +0200

    revert example

--- examples/create_document.py
+++ examples/create_document.py
@@ -2,20 +2,8 @@ import sys
 import config
 sys.path.append("..")
 import eversign
-"""
+
 client = eversign.Client(config.token)
-"""
-client = eversign.Client()
-
-oauth_request = {
-    'client_id': config.oauth_client_id,
-    'client_secret': config.oauth_client_secret,
-    'code': config.code,
-    'state': config.state
-}
-
-token = client.request_oauth_token(oauth_request)
-client.set_oauth_access_token(token)
 
 document = eversign.Document()
 document.title = "Tile goes here"
